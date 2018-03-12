fc247f53d28ffb34750dc1aa0c0a81b7e99cc790
Answer-Geng <Lil.Answer@outlook.com>
2018-03-12 17:08:23 +0800

    添加quartz

--- Lava.QuartzServer/Program.cs
+++ Lava.QuartzServer/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quartz.Impl;
+using System.Collections.Specialized;
+using Quartz;
 
 namespace Lava.QuartzServer
 {
@@ -10,7 +13,33 @@ namespace Lava.QuartzServer
     {
         static void Main(string[] args)
         {
+            NameValueCollection properties = new NameValueCollection
+            {
+                ["quartz.scheduler.instanceName"] = "Lava",
+                ["quartz.scheduler.instanceId"] = "Auto",
+                ["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz",
+                ["quartz.threadPool.threadCount"] = "5",
+                ["quartz.threadPool.threadPriority"] = "Normal",
+                ["quartz.scheduler.exporter.type"] = "Quartz.Simpl.RemotingSchedulerExporter, Quartz",
+                ["quartz.scheduler.exporter.port"] = "555",
+                ["quartz.scheduler.exporter.bindName"] = "QuartzScheduler",
+                ["quartz.scheduler.exporter.channelType"] = "tcp",
+                ["quartz.scheduler.exporter.channelName"] = "httpQuartz",
+                ["quartz.scheduler.exporter.rejectRemoteRequests"] = "true",
+                ["quartz.jobStore.misfireThreshold"] = "60000",
+                ["quartz.jobStore.type"] = "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz",
+                ["quartz.jobStore.dataSource"] = "default",
+                ["quartz.jobStore.tablePrefix"] = "LAVA_",
+                ["quartz.jobStore.clustered"] = "true",
+                ["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz",
+                ["quartz.dataSource.default.connectionString"] = "Data Source=.;Initial Catalog=Lava;User ID=sa;Password=123",
+                ["quartz.dataSource.default.provider"] = "SqlServer-20"
+            };
 
+            ISchedulerFactory sf = new StdSchedulerFactory(properties);
+            IScheduler sched = sf.GetScheduler();
+            sched.Start();
         }
+
     }
 }
